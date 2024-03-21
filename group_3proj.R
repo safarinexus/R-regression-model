f1da33e76ba1c6dae9dc01cc38e26da7089f5520
Edgar <edgargarteong@gmail.com>
2024-03-21 14:02:38 +0800

    cleaned up script

--- group_3proj.R
+++ group_3proj.R
@@ -28,30 +28,27 @@ base_clean2 <- base_clean1 %>% filter(!is.na(lt)) #remove rows where total liabi
 #assumption:
 base_clean3 <- base_clean2 %>% mutate(clean_act = ifelse(is.na(act), che + rect + invt ,act)) 
 #assumption:
-base_clean4 <- base_clean3 %>% filter(!is.na(sale))
+base_clean4 <- base_clean3 %>% filter(!is.na(sale)) #remove zero sales because
 #assumption: 
-base_clean5 <- 
+base_clean5 <- base_clean4 %>% filter(!is.na(oancf)) #remove zero operating net cash flow because
+#assumption: 
+base_clean6 <-
 
+#final cleaned data renaming 
+base1 <- base_cleanx
 #===========ADDING VARIABLES==================#
 #adding gross profit variable 
-base1 <- base %>%
+base2 <- base1 %>%
   arrange(cid, fyear) %>%
   mutate(gpm = (sale-cogs) / sale) %>% ungroup() 
 
 #adding long-term debt  
-base2 <- base1 %>% arrange(cid, fyear) %>% group_by(cid) %>% mutate(dltt_lag = lag(dltt, n = 1)) 
-base3 <- base2 %>% mutate(dltt_change = dltt - dltt_lag) %>% mutate(dltt_dummy = ifelse(dltt_change > 0, 0, 1))
-basedltt <- filter(base3, dltt == 0) #no 0, so assuming NA = 0 dltt
-basetest <- base3 %>% group_by(fyear) %>% select(
-  c(datadate, at, cid, fyear, fyr, dltt, lt, dlc, txp, txdb, lct)) 
-baseyourmom <- basetest %>% filter(is.na(lt))
-View(basetest)
-View(baseyourmom)
+base3 <- base2 %>% arrange(cid, fyear) %>% group_by(cid) %>% mutate(dltt_lag = lag(dltt, n = 1)) 
+base4 <- base3 %>% mutate(dltt_change = dltt - dltt_lag) %>% mutate(dltt_dummy = ifelse(dltt_change > 0, 0, 1))
+basedltt <- filter(base4, dltt == 0) #no 0, so assuming NA = 0 dltt
 
 #================CLEANING DATA=================#
 #dltt data cleaning 
-
-
 base_final_cleaned = na.omit(base_final)
 
 
