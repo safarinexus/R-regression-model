6e8170677ddc0bfb5fbcf09ab9c0e2986f3dc97e
Edgar <edgargarteong@gmail.com>
2024-03-21 13:42:55 +0800

    added initial script with data cleaning and added variables

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # R_regression_model
+ACCT337 Statistical Programming (R)
+
+Logistic Regression and Decision Tree of the likelihood of firms meeting or beating market expectations
+
---
+++ group_3proj.R
@@ -0,0 +1,48 @@
+options(scipen=999, digits=4) # avoid scientific display, keep 4 digits in display
+rm(list=ls()) # clear Environment
+setwd("/Users/edgr/Library/Mobile Documents/com~apple~CloudDocs/SMU tings/current/statprog/statprog proj")
+
+#===========PACKAGES & DEPENDENCIES===========#
+install.packages('caret', dependencies = TRUE)
+install.packages("corrplot")
+install.packages("glmnet")
+library(caret)
+library(dplyr)
+library(readr)
+library(stats)
+library(rpart)
+library(glmnet)
+library(corrplot)
+base <- read_csv("teamproject_final.csv")
+#===========PRELIMNARY CLEANING==============#
+rebase <- base %>% filter(!is.na(at)) #remove rows where total assets is NA
+rebase1 <- rebase %>% filter(!is.na(ceq)) #remove rows where equity is NA
+rebase2 <- rebase %>% filter(!is.na(lt)) #remove rows where total liabilities is NA
+View(rebase2)
+
+#===========ADDING VARIABLES=================#
+#adding gross profit variable 
+base1 <- base %>%
+  arrange(cid, fyear) %>%
+  mutate(gpm = (sale-cogs) / sale) %>% ungroup() 
+
+#adding long-term debt  
+base2 <- base1 %>% arrange(cid, fyear) %>% group_by(cid) %>% mutate(dltt_lag = lag(dltt, n = 1)) 
+base3 <- base2 %>% mutate(dltt_change = dltt - dltt_lag) %>% mutate(dltt_dummy = ifelse(dltt_change > 0, 0, 1))
+basedltt <- filter(base3, dltt == 0) #no 0, so assuming NA = 0 dltt
+basetest <- base3 %>% group_by(fyear) %>% select(
+  c(datadate, at, cid, fyear, fyr, dltt, lt, dlc, txp, txdb, lct)) 
+baseyourmom <- basetest %>% filter(is.na(lt))
+View(basetest)
+View(baseyourmom)
+
+#================CLEANING DATA================#
+#dltt data cleaning 
+
+
+base_final_cleaned = na.omit(base_final)
+
+
+#=======REMOVING UNNECESSARY COLUMNS=========#
+base4 <- select(base3, c(datadate, cid, fyear, fyr, mob, gpm, dltt_dummy, n_aef, ghg))
+View(base4)
